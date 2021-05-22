0e0bc505a684e43b12294cf99b31b68f78e2f863
conor36 <41356276+conor36@users.noreply.github.com>
2021-05-22 20:25:45 +0100

    Add files via upload

---
+++ explore.py
@@ -0,0 +1,101 @@
+import streamlit as st
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def shorten_categories(categories, cutoff):
+    categorical_map = {}
+    for i in range(len(categories)):
+        if categories.values[i] >= cutoff:
+            categorical_map[categories.index[i]] = categories.index[i]
+        else:
+            categorical_map[categories.index[i]] = 'Other'
+    return categorical_map
+
+
+@st.cache
+
+def load_data():
+    df = pd.read_csv("investmentVC.csv") 
+    df = df[[" market ", " funding_total_usd ", "country_code", "founded_year", "status"]]
+    df = df.rename({" market ": "market", " funding_total_usd ": "funding"}, axis=1)
+    df['funding'] = df['funding'].str.replace(',', '').str.replace(' -   ', '').str.replace(' ', '').str.replace('  ', '').str.replace('   ', '').str.replace('  ', '').str.replace('    ', '')
+    df.replace('', np.nan, inplace = True)
+    df = df.dropna()
+    df['funding'] = df['funding'].astype(float)
+    df['market'] = df['market'].str.replace(' ', '')
+    df = df.loc[df['market'].isin(['Software',
+ 'Biotechnology',
+ 'Mobile',
+ 'E-Commerce',
+ 'CuratedWeb',
+ 'EnterpriseSoftware',
+ 'HealthCare',
+ 'Hardware+Software',
+ 'Advertising',
+ 'Games',
+ 'CleanTechnology',
+ 'HealthandWellness',
+ 'SocialMedia',
+ 'Education',
+ 'Finance'])]
+    
+    df.drop(index=df[df['status'] == 'operating'].index, inplace=True)
+    country_map = shorten_categories(df.country_code.value_counts(), 17)
+    df['country_code'] = df['country_code'].map(country_map)
+    print(df)
+    return df
+
+
+df = load_data()
+
+def show_explore_page():
+
+    st.title("Explore Startup Data")
+
+    st.write("""## This page is a little exploreation of the datat provided by Crunchbase for startups around the world up until 2013""")
+
+    st.write("""### The full dataset can be found [here](https://www.kaggle.com/arindam235/startup-investments-crunchbase)""")
+
+    data = df["status"].value_counts()
+
+    fig1, ax1 = plt.subplots()
+    colors = ['#66b3ff','#ff9999']
+    ax1.pie(data, labels=data.index, autopct="%1.1f%%", colors= colors, shadow=False)
+    ax1.axis("equal")
+    
+    
+    st.write("""
+    ### Number of Data from different countries
+    """)
+
+    st.pyplot(fig1)
+    
+    st.write(
+    
+    """
+    
+    ### Mean Funding Based On Country
+
+    """
+    )
+
+    data = df.groupby(["country_code"])["funding"].mean().sort_values(ascending=True)
+    st.bar_chart(data)
+    
+    st.write(
+    """
+    
+    ### Mean Funding Based On Market
+
+    """
+    )
+    data2 = df.groupby(["market"])["funding"].mean().sort_values()
+    st.bar_chart(data2)
+
+
+
+
+  
---
+++ predict.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+model_loaded = data["model"]
+market_fit = data["market_fit"]
+country_fit = data["country_fit"]
+
+def show_predict_page():
+    st.title("Predict if a Startup Will be Succesful")
+
+    st.write("""#### This a project using Random Forest Classifaction model to determine if a startup will be acquired or closed based on the several values below. For a full breakdwon on how this was made, feel free to visit my medium article [here]. The full dataset can be found [here](https://www.kaggle.com/arindam235/startup-investments-crunchbase).""")
+
+    st.write("""#### By [Conor Reilly](http://www.conorreilly.com)""")
+
+    st.write("""### Fill out the info below to complete prediction""")
+
+    markets = ('E-Commerce', 'HealthandWellness', 'Education', 'CuratedWeb',
+       'Software', 'Biotechnology', 'Mobile', 'Advertising', 'Games',
+       'Finance', 'SocialMedia', 'EnterpriseSoftware', 'CleanTechnology',
+       'Hardware+Software', 'HealthCare')
+
+    countries = ('USA', 'Other', 'GBR', 'CAN', 'FRA', 'CHN', 'IND', 'DEU', 'ISR', 'ESP')
+
+    market = st.selectbox("Company Market", markets)
+
+    country = st.selectbox("Country of origin", countries)
+
+    funding = st.slider("Funding in total (USD)", 0, 10000000, 100000, step=100000)
+
+    founded_year = st.slider("Year company was founded", 1995, 2013, 1995, step=1)
+
+    ok = st.button("Predict Startup Future")
+    if ok:
+        X = np.array([[market, funding, country, founded_year]])
+        X[:, 0] = market_fit.transform(X[:,0])
+        X[:, 2] = country_fit.transform(X[:,2])
+        X = X.astype(float) 
+
+        status = model_loaded.predict(X)
+        st.subheader(f"The status for this company is predicted to be {status}")
+
+    
