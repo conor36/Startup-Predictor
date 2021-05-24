05d651a0d8155866d851d2707174c12da15aa421
conor36 <41356276+conor36@users.noreply.github.com>
2021-05-24 22:30:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from predict import show_predict_page
 from explore import show_explore_page
 
-st.set_page_config(page_title="Startup Predictor :rocket:", page_icon=":rocket:")
+st.set_page_config(page_title="Startup Predictor", page_icon=":rocket:")
 page = st.sidebar.selectbox("Explore or Predict", ("Predict", "Explore"))
 
 if page == "Predict":
