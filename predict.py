184984474b4f7be805557e0a3687ca76ebfcf9a0
conor36 <41356276+conor36@users.noreply.github.com>
2021-05-24 22:10:40 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -19,9 +19,9 @@ def show_predict_page():
     footer {visibility: hidden;}
     </style> """, unsafe_allow_html=True)
     
-    st.set_page_config(page_title=’Startup Predictor’, page_icon=”🚀”)
+    st.set_page_config(page_title=’Startup Predictor’, page_icon="rocket":"🚀")
     
-    st.title("Predict if a Startup Will be Succesful🚀")
+    st.title("Predict if a Startup Will be Succesful "rocket":"🚀")
 
     st.write("""#### This a project using Random Forest Classifaction model to determine if a startup will be acquired or closed based on the several values below. For a full breakdwon on how this was made, feel free to visit my medium article [here]. The full dataset can be found [here](https://www.kaggle.com/arindam235/startup-investments-crunchbase).""")
 
